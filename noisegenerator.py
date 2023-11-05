cbb8d5a81e735e08cec96f8b56f439da0b0a522f
EduardoOrtizRosas <eortizro@asu.edu>
2023-11-05 15:31:42 -0700

    Update noisegenerator.py
    simple connect to a server, change if incorrect

--- noisegenerator.py
+++ noisegenerator.py
@@ -7,12 +7,23 @@ import random
 import threading
 import time
 
+
 class NoiseGenerator:
-    
+    HOST = ""
+    PORT = 0
     def __init__(self):
         pass
 
 def main():
+    """
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST,PORT))
+        s.sendall("Hi")
+        data = s.recv(1024)
+
+    """
+
+    
     pass
 
 
