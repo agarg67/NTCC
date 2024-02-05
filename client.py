499c5149d10352bb53e128e19d51d7095893c915
EduardoOrtizRosas <eortizro@asu.edu>
2024-02-05 11:22:49 -0700

    Update client.py

--- client.py
+++ client.py
@@ -17,7 +17,7 @@ class Client:
     clientCentralPort=0  
     clientRelayPort=0
 
-    centralServerIp="10.157.125.185"
+    centralServerIp="192.168.244.140"
     centralServerPort=20001 # port is fixed up
     
     relayServerIpList=[]
