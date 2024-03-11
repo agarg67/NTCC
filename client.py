f5ef3516e0b94e1641fdff01706dcbeba34acaab
agarg67 <99454497+agarg67@users.noreply.github.com>
2024-03-11 13:59:35 -0700

    Update client.py

--- client.py
+++ client.py
@@ -18,7 +18,7 @@ class Client:
     clientCentralPort=0  
     clientRelayPort=0
 
-    centralServerIp="192.168.19.37"
+    centralServerIp="10.155.131.118"
     centralServerPort=20001 # port is fixed up
 
     forwarderServerIp="localhost"
